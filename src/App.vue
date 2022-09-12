f86618c413adda2f9bc7beea8dbeabbad4926a3f
Artem <artem@inisev.com>
2022-09-12 10:52:48 +0200

    devchalleng buttons  completed

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import MyButton from "./components/MyButton.vue";
+</script>
+
+<template>
+  <p><code>MyButton default</code></p>
+  <MyButton> </MyButton>
+  <p><code>MyButton default medium</code></p>
+  <MyButton size="md"></MyButton>
+  <p><code>MyButton default large</code></p>
+  <MyButton size="lg"></MyButton>
+  <p><code>MyButton default large no-shadow </code></p>
+  <MyButton
+    size="lg"
+    :is-shadow="false"
+  ></MyButton>
+  <p><code>MyButton default primary</code></p>
+  <MyButton color="primary"></MyButton>
+  <p><code>MyButton default secondary</code></p>
+  <MyButton color="secondary">
+    <template v-slot:text>Secondary</template>
+  </MyButton>
+  <p><code>MyButton default danger</code></p>
+  <MyButton color="danger">
+    <template v-slot:text>Danger</template>
+  </MyButton>
+  <p><code>btn outline</code></p>
+  <MyButton :variant="'outline'">
+    <template v-slot:text>Outline</template>
+  </MyButton>
+  <p><code>btn text</code></p>
+  <MyButton :variant="'text'">
+    <template v-slot:text>Btn text</template>
+  </MyButton>
+  <MyButton
+    :variant="'text'"
+    :icon-position="'end'"
+    :end-icon="'double_arrow'"
+  >
+    <template v-slot:text>Link text</template>
+  </MyButton>
+
+  <p><code>btns with icon</code></p>
+  <div style="display: flex; flex-wrap: wrap; align-items: center; justify-content: center; gap: 2rem">
+    <MyButton
+      color="primary"
+      size="md"
+      :end-icon="'add_shopping_cart'"
+    >
+      <template v-slot:text>Add to cart</template>
+    </MyButton>
+
+    <MyButton
+      :icon-position="'end'"
+      color="danger"
+      size="md"
+      :start-icon="'add_shopping_cart'"
+    >
+      <template v-slot:text>Remove from cart</template>
+    </MyButton>
+  </div>
+
+  <p><code>MyButton default disabled</code></p>
+  <MyButton :is-disabled="true"> </MyButton>
+</template>
+
+<style scoped></style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,198 @@
+<script>
+export default {
+  name: "MyButton",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    variant: {
+      type: String,
+      default: "Default",
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "sm",
+    },
+    color: {
+      type: String,
+      default: "",
+    },
+    iconPosition: {
+      type: String,
+      default: "start",
+    },
+    isShadow: {
+      type: Boolean,
+      default: true,
+    },
+    startIcon: {
+      type: String,
+      default: "",
+    },
+    endIcon: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    getVariantClassName() {
+      let className = "";
+      switch (this.variant) {
+        case "outline":
+          return (className = "outline");
+          break;
+        case "text":
+          return (className = "btn-text");
+          break;
+        default:
+          break;
+      }
+      return className;
+    },
+    getSizeClassName() {
+      let className = "";
+      switch (this.size) {
+        case "md":
+          return (className = "medium");
+          break;
+        case "lg":
+          return (className = "large");
+          break;
+        default:
+          break;
+      }
+      return className;
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    :type="type"
+    :class="[
+      getVariantClassName,
+      getSizeClassName,
+      `${color}`,
+      isShadow === false ? 'not-shadow' : '',
+    ]"
+    :disabled="isDisabled"
+  >
+    <span
+      v-if="startIcon !== ''"
+      class="material-symbols-outlined"
+    >
+      {{ startIcon }}
+    </span>
+    <slot name="text">Default</slot>
+    <span
+      v-if="endIcon !== ''"
+      class="material-symbols-outlined"
+    >
+      {{ endIcon }}
+    </span>
+  </button>
+</template>
+
+<style lang="scss">
+button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 8px;
+  border: 1px solid transparent;
+  padding: 0.42rem 0.86rem;
+  font-size: 1rem;
+  font-weight: 500;
+  font-family: inherit;
+  cursor: pointer;
+  background: #e0e0e0;
+  box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+  border-radius: 6px;
+}
+.medium {
+  padding: 0.57rem 1.142rem;
+}
+.large {
+  padding: 0.786rem 1.57rem;
+}
+
+button:hover:not(:disabled) {
+  background-color: #aeaeae;
+}
+button:focus:not(:disabled),
+button:focus-visible:not(:disabled) {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+button:disabled {
+  box-shadow: none;
+  color: #9e9e9e;
+  &:hover {
+    cursor: not-allowed;
+  }
+}
+@media (prefers-color-scheme: light) {
+  button {
+    background-color: #f9f9f9;
+  }
+}
+.outline {
+  border-color: var(--primary-color);
+  color: var(--primary-color);
+  background-color: #fff;
+  box-shadow: none;
+}
+.outline:hover:not(:disabled) {
+  background-color: rgba(41, 98, 255, 0.1);
+}
+.outline:focus:not(:disabled),
+.outline:focus-visible:not(:disabled) {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+.btn-text {
+  border-color: transparent;
+  color: var(--primary-color);
+  background-color: #fff;
+  box-shadow: none;
+}
+.btn-text:hover:not(:disabled) {
+  background-color: rgba(41, 98, 255, 0.1);
+}
+.btn-text:focus:not(:disabled),
+.btn-text:focus-visible:not(:disabled) {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+.primary {
+  color: #fff;
+  background: #2962ff;
+  box-shadow: 0px 2px 3px rgba(41, 98, 255, 0.2);
+  &:hover:not(:disabled) {
+    background-color: #0039cb;
+  }
+}
+.secondary {
+  color: #fff;
+  background: #455a64;
+  box-shadow: 0px 2px 3px rgba(69, 90, 100, 0.2);
+  &:hover:not(:disabled) {
+    background-color: #1c313a;
+  }
+}
+.danger {
+  color: #fff;
+  background: #d32f2f;
+  box-shadow: 0px 2px 3px rgba(211, 47, 47, 0.2);
+  &:hover:not(:disabled) {
+    background-color: #9a0007;
+  }
+}
+.not-shadow {
+  box-shadow: none;
+}
+</style>
